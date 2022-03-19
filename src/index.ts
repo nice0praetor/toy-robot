942b88f3868416488384d9b6c4fdbbe615c25712
John Smith <nice0.praetor@icloud.com>
2022-03-19 14:38:50 +1030

    Add CLI entrypoint and line reading

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello");
+import readline from 'readline';
+import { parseCommand } from './lib/command';
+import { GameState } from './lib/state';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+
+
+const game = new GameState();
+console.log("Game ready. Ctrl-c to exit.\n\nPlease type your command\n")
+rl.on('line', function(line){
+    const command = parseCommand(line);
+    if (command) {
+        game.perform(command);
+    }
+})
